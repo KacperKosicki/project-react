81ec291705b99985e3429cbeef33498fa68a87db
Kacper Kosicki <kosickikacper1@gmail.com>
2023-07-27 17:59:50 +0200

    Add all needed classes to the project. Code setup.

--- src/components/SearchForm/SearchForm.js
+++ src/components/SearchForm/SearchForm.js
@@ -6,9 +6,11 @@ const SearchForm = () => {
     return (
         <form className={styles.searchForm}>
             <TextInput placeholder="Search..." />
-            <Button text="Search" />
+            <Button>
+                <span className="fa fa-search" />
+            </Button>
         </form>
     );
-};
+  };
 
 export default SearchForm;
