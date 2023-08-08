9b9f56b0611b1f5323d39f83c237bb0dc9fd6f1d
Kacper Kosicki <kosickikacper1@gmail.com>
2023-08-08 12:09:56 +0200

    Add navigation to the project

--- src/App.js
+++ src/App.js
@@ -2,14 +2,21 @@ import Hero from './components/Hero/Hero'
 import SearchForm from './components/SearchForm/SearchForm'
 import List from './components/List/List';
 import Container from './components/Container/Container';
+import NavBar from './components/NavBar/NavBar';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 const App = () => {
   return (
-    <Container>
-      <Hero />
-      <SearchForm />
-      <List />
-    </Container>
+    <Router>
+      <NavBar />
+      <main>
+        <Container>
+          <Hero />
+          <SearchForm />
+          <List />
+        </Container>
+      </main>
+    </Router>
   );
 };
 
---
+++ src/components/NavBar/NavBar.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import styles from './NavBar.module.scss';
+import Container from '../Container/Container';
+
+const NavBar = () => {
+    return (
+        <nav className={styles.navBar}>
+        <Container>
+          <div className={styles.navContent}>
+            <a href="/" className={styles.logo}>
+              <span className="fa fa-tasks" style={{ fontSize: '24px' }} />
+            </a>
+            <ul className={styles.navLinks}>
+              <li>
+                <a href="/">Home</a>
+              </li>
+              <li>
+                <a href="/favorite">Favorite</a>
+              </li>
+              <li>
+                <a href="/about">About</a>
+              </li>
+            </ul>
+          </div>
+        </Container>
+      </nav>
+    );
+  };
+  
+  export default NavBar;
